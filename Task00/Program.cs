f537db0edc56527e7178a2b99edd1fa918893006
Fufaev <georijf@bk.ru>
2023-02-01 21:00:00 +0500

    add Task 00 for example

---
+++ Task00/Program.cs
@@ -0,0 +1,6 @@
+﻿// Программа, возводящая число в квадрат.
+
+Console.Write("Введите любое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int square = number * number;
+Console.WriteLine($"Квадрат введенного вами числа = {square}");
