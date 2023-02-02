fdf63a6dc9a92a5dfc71a5c398e516dc245f1457
Fufaev <georijf@bk.ru>
2023-02-02 22:04:00 +0500

    Added Task 07 Example

---
+++ Task07/Program.cs
@@ -0,0 +1,16 @@
+﻿/* 7.Напишите программу, которая принимает на вход 
+трёхзначное число и на выходе показывает последнюю 
+цифру этого числа */
+
+Console.Write("Введите трёхзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number > 99 && number <= 999)
+{
+    Console.WriteLine("Число трёхзначное");
+    int res = number % 10;
+    Console.WriteLine($"Последняя цифра числа - это: {res}");
+}
+else
+{
+    Console.WriteLine("Число не трёхзначное");
+}
