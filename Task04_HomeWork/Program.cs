38c0a590043812b21f0323eab256ab8e902019bf
Fufaev <georijf@bk.ru>
2023-02-03 05:18:29 +0500

    Added Task 04 Home Work

---
+++ Task04_HomeWork/Program.cs
@@ -0,0 +1,24 @@
+﻿/* Задача 4: Напишите программу, которая принимает 
+на вход три числа и выдаёт 
+максимальное из этих чисел.
+
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22 */
+
+Console.Write("Введите первое число: ");
+int numb1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите первое число: ");
+int numb2 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите первое число: ");
+int numb3 = Convert.ToInt32(Console.ReadLine());
+
+int max = numb1;
+
+if (numb1 > max) max = numb1;
+if (numb2 > max) max = numb2;
+if (numb3 > max) max = numb3;
+
+Console.Write($"Максимально введенне значение = {max}");
