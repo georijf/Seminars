6205035d7d5b9fcfb9902f6fd68e728d1c91e926
Fufaev <georijf@bk.ru>
2023-02-05 18:06:44 +0500

    Added Task 09 Method Example

---
+++ Task09/Program.cs
@@ -0,0 +1,21 @@
+﻿/* 9. Напишите программу, которая выводит
+случайное число из отрезка [10, 99] и показывает
+наибольшую цифру числа.
+78 -> 8
+12-> 2
+85 -> 8 */
+
+int numb = new Random().Next(10, 100);
+Console.WriteLine($"Рандомное число в диопазоне от 10 до 99: {numb}");
+
+int maxDigit = MaxDigit(numb);
+Console.WriteLine($"Наибольшая цифра в числе: {maxDigit}");
+
+
+int MaxDigit(int num)
+{
+    int first = num / 10;
+    int second = num % 10;
+    int result = first > second ? first : second;
+    return result;
+}
