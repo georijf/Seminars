97ea8f292644d798963db6fc0ff7ad8e662400d5
Fufaev <georijf@bk.ru>
2023-02-05 19:15:01 +0500

    Added Task 11 Example

---
+++ Task11/Program.cs
@@ -0,0 +1,13 @@
+﻿int number = new Random().Next(100, 1000);
+Console.WriteLine($"Рандомное трехзначное число: {number}");
+
+int CutNum = Cut(number);
+Console.WriteLine($"Цифра из первого и последнего числа трехначной цифры: {CutNum}");
+
+int Cut(int num)
+{
+    int first = num / 100;
+    int last = num % 10;
+    int result = first * 10 + last;
+    return result;
+}
