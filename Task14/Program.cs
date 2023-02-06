42f1fe92a400358165bdb650285429373ca50163
Fufaev <georijf@bk.ru>
2023-02-06 22:08:58 +0500

    Added Task 14 Example

---
+++ Task14/Program.cs
@@ -0,0 +1,17 @@
+﻿/* 14. Напишите программу, которая принимает на
+вход число и проверяет, кратно ли оно
+одновременно 7 и 23.
+14 -> нет
+46 -> нет
+161 -> да */
+
+bool Mult(int number)
+{
+    return number % 7 == 0 && number % 23 == 0;
+}
+
+Console.Write("Введите число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+bool mult = Mult(num1);
+Console.WriteLine(mult ? "ДА" : "НЕТ");
+
