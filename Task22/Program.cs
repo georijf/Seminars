d9de89743b836765fc5b3be35156b9f9c0d7d1c8
Fufaev <georijf@bk.ru>
2023-02-11 19:26:13 +0500

    Added Task 22 Example

---
+++ Task22/Program.cs
@@ -0,0 +1,23 @@
+﻿/* Задача 22: Напишите программу, которая
+принимает на вход число (N) и выдаёт таблицу
+квадратов чисел от 1 до N.
+5 -> 1, 4, 9, 16, 25.
+2 -> 1,4 */
+
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Число\tТаблица квадратов");
+SquareTable(number);
+
+
+void SquareTable(int num)
+{
+    int count = 1;
+    while (count <= num)
+    {
+        Console.WriteLine($"{count}\t{count * count}");
+        count++;
+    }
+
+}
