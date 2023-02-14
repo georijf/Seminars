279ef4397de3600aee1c2427d6a92f07a16d9061
Fufaev <georijf@bk.ru>
2023-02-14 19:21:27 +0500

    Added Task 24 Example

---
+++ Task24/Program.cs
@@ -0,0 +1,23 @@
+﻿/* Задача 24: Напишите программу, которая
+принимает на вход число (А) и выдаёт сумму чисел
+от 1 до А.
+7 -> 28
+4 -> 10
+8 -> 36 */
+
+
+Console.WriteLine("Введите целое положительное число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int sumNumbers = SumNumpers(number);
+Console.WriteLine($"Сумма чисел от 1 до {number} = {sumNumbers}");
+
+int SumNumpers(int num)
+{
+    int sum = 0;
+    for (int i = 0; i <= num; i++)
+    {
+        sum = sum + i;
+    }
+    return sum;
+}
