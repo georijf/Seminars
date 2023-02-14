3a8f56511ca7f4d0a00ff2f1a5aec94732c41fa5
Fufaev <georijf@bk.ru>
2023-02-14 19:58:52 +0500

    Added Task 26 Example

---
+++ Task26/Program.cs
@@ -0,0 +1,23 @@
+﻿/* Задача 26: Напишите программу, которая принимает
+на вход число и выдаёт количество цифр в числе.
+456 -> 3
+78 -> 2
+89126 -> 5 */
+
+int GetAll(int number)
+{
+    if (number < 0) number = number * -1;
+    int count = 0;
+    while (number > 0)
+    {
+        number = number / 10;
+        count++;
+    }
+    return count;
+}
+
+Console.Write("Введите число чтобы получить количество цифр в нём: ");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Количество цифр в этом числе: {GetAll(num)}");
+
+
