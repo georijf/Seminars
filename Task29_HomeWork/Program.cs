a5a1f10ff904d95c63556c2c183bd9e159dc724a
Fufaev <georijf@bk.ru>
2023-02-14 21:46:04 +0500

    Added Task 29 HomeWork

---
+++ Task29_HomeWork/Program.cs
@@ -0,0 +1,15 @@
+﻿/* Задача 29: 
+Напишите программу, которая задаёт 
+массив из 8 элементов и выводит их на экран.
+
+1, 2, 5, 7, 19, 6, 1, 33 -> [1, 2, 5, 7, 19, 6, 1, 33] */
+
+Console.WriteLine("Длинну массива: ");
+int VstArray = Convert.ToInt32(Console.ReadLine()); ;
+
+int[] randomArray = new int[VstArray];
+for (int i = 0; i < randomArray.Length; i++)
+{
+    randomArray[i] = new Random().Next(1, 9);
+    Console.Write(randomArray[i] + " ");
+}
