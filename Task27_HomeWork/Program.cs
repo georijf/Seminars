30d74555823f1d059817a65271e13206cc108995
Fufaev <georijf@bk.ru>
2023-02-14 21:21:21 +0500

    Added Task 27 Hone Work

---
+++ Task27_HomeWork/Program.cs
@@ -0,0 +1,34 @@
+﻿/* Задача 27: 
+Напишите программу, которая принимает 
+на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12 */
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int vst = NumberVst(number);
+SumNumbers(number, vst);
+
+int NumberVst(int a)
+{
+    int index = 0;
+    while (a > 0)
+    {
+        a /= 10;
+        index++;
+    }
+    return index;
+}
+void SumNumbers(int n, int vst)
+{
+    int sum = 0;
+    for (int i = 1; i <= vst; i++)
+    {
+        sum += n % 10;
+        n /= 10;
+    }
+    Console.WriteLine($"сумма цифр в числе = {sum}");
+}
+
