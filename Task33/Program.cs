ed513ab3604250f6e4c190353e8850c9620977a6
Fufaev <georijf@bk.ru>
2023-02-17 19:58:45 +0500

    Added Task 33 Example

---
+++ Task33/Program.cs
@@ -0,0 +1,46 @@
+﻿/* Задача 33: Задайте массив. Напишите программу, которая
+определяет, присутствует ли заданное число в массиве.
+4; массив [6, 7, 19, 345, 3] -> нет
+3; массив [6, 7, 19, 345, 3] -> да */
+
+
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] array = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(min, max + 1);
+    }
+    return array;
+}
+
+bool CheckArray(int num, int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] == num) return true;
+    }
+    return false;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]} ");
+    }
+    Console.Write("]");
+}
+
+Console.Write("Введиче число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int[] massiv = CreateArrayRndInt(5, 0, 5);
+PrintArray(massiv);
+
+bool check = CheckArray(num, massiv);
+Console.WriteLine(check ? " -> Да" : " -> Нет");
